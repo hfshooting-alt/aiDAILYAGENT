2b15eb627cd7717ecfb9fff0cad5b2b871a805f8
hfshooting-alt <hfshooting@163.com>
2026-03-05 11:49:36 +0800

    feat: update weibo target list and derive uid locks from profile links

---
+++ src/targets.js
@@ -0,0 +1,27 @@
+export const X_TARGET_PROFILES = [
+  { name: 'Jim Fan', handle: 'jimfan', homepage: 'https://x.com/jimfan' },
+  { name: 'Andrej Karpathy', handle: 'karpathy', homepage: 'https://x.com/karpathy' },
+  { name: 'Ashok Elluswamy', handle: 'aelluswamy', homepage: 'https://x.com/aelluswamy' },
+  { name: 'Yann LeCun', handle: 'ylecun', homepage: 'https://x.com/ylecun' },
+  { name: 'Sam Altman', handle: 'sama', homepage: 'https://x.com/sama' },
+  { name: 'Harrison Chase', handle: 'hwchase17', homepage: 'https://x.com/hwchase17' },
+  { name: 'swyx', handle: 'swyx', homepage: 'https://x.com/swyx' },
+  { name: 'Clement Delangue', handle: 'ClementDelangue', homepage: 'https://x.com/ClementDelangue' },
+  { name: 'Demis Hassabis', handle: 'demishassabis', homepage: 'https://x.com/demishassabis' },
+  { name: 'Ethan Mollick', handle: 'emollick', homepage: 'https://x.com/emollick' }
+];
+
+export const WEIBO_TARGET_PROFILES = [
+  { name: '周鸿祎', homepage: 'https://m.weibo.cn/u/1708942053' },
+  { name: '宝玉xp', homepage: 'https://m.weibo.cn/u/1727858283?t=0&luicode=10000011&lfid=1076031727858283&launchid=10000360-page_H5' },
+  { name: '数字生命卡兹克', homepage: 'https://m.weibo.cn/u/5700099573?t=0&luicode=10000011&lfid=231583&launchid=10000360-page_H5' },
+  { name: '阑夕', homepage: 'https://m.weibo.cn/u/1560906700?t=0&luicode=10000011&lfid=100505172785828&launchid=10000360-page_H5' },
+  { name: '归藏的AI漫游指南', homepage: 'https://m.weibo.cn/u/6182606334' },
+  { name: '爱可可-爱生活', homepage: 'https://m.weibo.cn/u/1402400261?t=0&luicode=10000011&lfid=1076036182606334&launchid=10000360-page_H5' },
+  { name: '稚晖君', homepage: 'https://m.weibo.cn/u/1679186305?t=0&luicode=10000011&lfid=1076031402400261&launchid=10000360-page_H5' },
+  { name: '塞冬的生活日记', homepage: 'https://m.weibo.cn/u/1706699904?luicode=10000011&lfid=1005051706699904&launchid=10000360-page_H5' },
+  { name: '王小川', homepage: 'https://m.weibo.cn/u/1582488432?t=0&luicode=10000011&lfid=1076032191707743&launchid=10000360-page_H5' }
+];
+
+export const X_TARGETS = X_TARGET_PROFILES.map((item) => item.handle);
+export const WEIBO_TARGETS = WEIBO_TARGET_PROFILES.map((item) => item.name);
